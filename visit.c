6277e84103657df4a5d57589fef02d54abc096b2
Mark Renard <marng7@mail.umsl.edu>
2020-02-09 13:06:17 -0600

    Type information and permission options work

--- visit.c
+++ visit.c
@@ -8,34 +8,62 @@
 #include <string.h>
 #include <sys/stat.h>
 
-static void addType(char *, struct stat *);
+static struct stat * getStats(char * path);
+static void printType(struct stat *);
+static void printPermissions(struct stat*);
 
 void visit(char * path, Options opts){
-	char * line;		// The line to be printed
 	struct stat * statPtr;  // Pointer to a stat structure
 
-	// Initializes line
-	line = (char *) malloc(10000);
-	strcpy(line, "aaa");	
-	printf("line:   %s\n", line);
-
 	// Gets reference to stat structure
-	lstat(path, statPtr);
+	statPtr = getStats(path);	
+	
+	if (opts.type) printType(statPtr);
+	if (opts.permissions) printPermissions(statPtr);
 
-	if (opts.type) addType(line, statPtr);
-
-	printf("line:   %s\n", line);
-	printf("path:   %s\n", path);
-	strcat(line, path);
-	printf("concat: %s\n\n", line);
-
+	printf(path);
+	printf("\n");
+	fflush(stdout);
+	
 }
 
-static void addType(char * line, struct stat * statPtr){
+static struct stat * getStats(char * path){
+	struct stat * statPtr = (struct stat *) malloc(sizeof(struct stat));
+
+	if (lstat(path, statPtr) == -1){
+		fprintf(stderr, "Couldn't get stats for %s\n", path);
+	}
+
+	return statPtr;
+}
+
+static void printType(struct stat * statPtr){
+	char type[2];				// Type to be printed
+	mode_t m = statPtr->st_mode;		// Mode member of stat struct
+
+	if (S_ISDIR(m)) strcpy(type, "d");
+	else if (S_ISREG(m)) strcpy(type, "-");
+
+	printf(type);
+}
+
+static void printPermissions(struct stat * statPtr){
+	char permissions[11];
 	mode_t m = statPtr->st_mode;
 
-	if (S_ISDIR(m)) strcat(line, "d");
-	else if (S_ISREG(m)) strcat(line, "-");
+	permissions[0] = (m & S_IRUSR ? 'r' : '-');
+	permissions[1] = (m & S_IWUSR ? 'w' : '-');
+	permissions[2] = (m & S_IXUSR ? 'x' : '-');
+	permissions[3] = (m & S_IRGRP ? 'r' : '-');
+	permissions[4] = (m & S_IWGRP ? 'w' : '-');
+	permissions[5] = (m & S_IXGRP ? 'x' : '-');
+	permissions[6] = (m & S_IROTH ? 'r' : '-');
+	permissions[7] = (m & S_IWOTH ? 'w' : '-');
+	permissions[8] = (m & S_IXOTH ? 'x' : '-');
+	permissions[9] = ' ';
+	permissions[10] = '\0';
+
+	printf(permissions);
 }
 
 
